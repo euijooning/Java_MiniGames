b7cefdd5af6f07756c70444bedcba2e4ef1cde1f
EUIJOON <euijoonism@gmail.com>
2023-11-07 17:53:14 +0900

    first commit

---
+++ src/test/java/games/minigames/MinigamesApplicationTests.java
@@ -0,0 +1,13 @@
+package games.minigames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinigamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
