9589b3300c6b64488c2abe9c56e6074021183051
EUIJOON <euijoonism@gmail.com>
2023-12-27 20:25:45 +0900

    [feat] 계산기 web

---
+++ src/main/java/games/calculator_web/CalculatorController.java
@@ -0,0 +1,47 @@
+package games.calculator_web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CalculatorController {
+
+    @GetMapping("/calculator")
+    public String showCalculator() {
+        return "calc";
+    }
+
+    @PostMapping("/calculate")
+    public String calculate(@RequestParam("num1") double num1,
+                            @RequestParam("num2") double num2,
+                            @RequestParam("operator") String operator,
+                            Model model) {
+        double result = 0;
+
+        switch (operator) {
+            case "add":
+                result = num1 + num2;
+                break;
+            case "subtract":
+                result = num1 - num2;
+                break;
+            case "multiply":
+                result = num1 * num2;
+                break;
+            case "divide":
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    model.addAttribute("error", "0으로는 나눌 수 없습니다.");
+                    return "calc";
+                }
+                break;
+        }
+
+        model.addAttribute("result", result);
+        return "calc";
+    }
+}
