9307a917b9b603aafd70f5fbd20b63afe14649b5
EUIJOON <euijoonism@gmail.com>
2023-12-27 20:29:36 +0900

    [refector] url 변경

--- src/main/java/games/calculator_web/CalculatorController.java
+++ src/main/java/games/calculator_web/CalculatorController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/calculator")
 public class CalculatorController {
 
-    @GetMapping("/calculator")
+    @GetMapping()
     public String showCalculator() {
         return "calc";
     }
